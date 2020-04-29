1b0d351d161cd725bfb0d0962f0fd5f19d008f7e
Alec Ross <asr3bj@virginia.edu>
2020-04-28 23:52:56 -0400

    changed file to work with a generic working directory

--- classes/data/dataPuller.py
+++ classes/data/dataPuller.py
@@ -9,7 +9,7 @@ import pandas as pd
 import os
 
 prefix = "C:\\Users\\Richard Wang\\Documents\\UVA 3 - Spring\\Information-Retrieval-Final-Project\\classes\\data\\csvs\\"
-
+path = os.getcwd() + "\\classes\\data\\csvs\\"  # generic path from the current working directory
 class book:
     def __init__(self):
         self.name = ""
@@ -18,18 +18,18 @@ class book:
 
 def pull_book_list():
     books = {}
-    df = pd.read_csv(prefix + "books.csv")
+    df = pd.read_csv(path + "\\books.csv")
     for i in range(len(df["book_id"])):
         books[df["book_id"][i]] = book()
         books[df["book_id"][i]].author = df["authors"][i]
         books[df["book_id"][i]].title = df["title"][i]
-    tag_map = pd.read_csv(prefix + "tags.csv")
+    tag_map = pd.read_csv(path + "\\tags.csv")
     tag_map = {key : value for (key, value) in zip(tag_map["tag_id"], tag_map["tag_name"])}
-    df = pd.read_csv(prefix + "book_tags.csv")
+    df = pd.read_csv(path + "book_tags.csv")
     for i in range(len(df["goodreads_book_id"])):
         books[df["goodreads_book_id"][i]].tags.append(tag_map[df["tag_id"][i]])
     return list(books.values())
 
 def pull_tag_list():
-    df = pd.read_csv(prefix + "tags.csv")
+    df = pd.read_csv(path + "tags.csv")
     return list(df["tag_name"])
